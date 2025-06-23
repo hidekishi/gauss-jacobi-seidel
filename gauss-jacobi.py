4ea13a0436a59cc1189faa1565538a511904c57f
hidekishi <caioshimohiro@hotmail.com>
2025-06-23 20:55:01 -0300

    Arquivo criado

---
+++ gauss-jacobi.py
@@ -0,0 +1,13 @@
+def x1(x1, x2, x3, x4):
+    return x2+x3+x4
+
+def x2(x1, x2, x3, x4):
+    return x1+x3+x4
+
+def x3(x1, x2, x3, x4):
+    return x1+x2+x4
+
+def x4(x1, x2, x3, x4):
+    return x1+x2+x3
+
+
